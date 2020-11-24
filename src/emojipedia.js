1a8c3dff478d270cc95cbc0403753aa4090c9aa4
DA-coder-ethio <64494269+DA-coder-ethio@users.noreply.github.com>
2020-11-24 15:20:58 +0200

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😘",
+    name: "Face Throwing a Kiss",
+    meaning:
+      "Smiley sends you a loving kiss that comes from the heart. Amicable, to express affection or with romantic intent, as an expression of love. May weaken a sarcastic response or be a reaction to a rude message. Symbol for flirting and showing gratitude."
+  },
+  {
+    id: 5,
+    emoji: "🐶",
+    name: "Dog Face",
+    meaning:
+      "A sweet dog face with a panting tongue and floppy ears. Stands for unconditional loyalty and attention. Can refer to your own pet (“I love my dog.“) or to people (“You sly dog!“)."
+  },
+  {
+    id: 6,
+    emoji: "🙊",
+    name: "Speak-No-Evil Monkey",
+    meaning:
+      "Monkey is holding both hands over its mouth. Keeps a secret, will not talk about something, wants to speak no evil or what it just said slipped out of its mouth. Iwazaru is part of the three wise monkeys."
+  }
+];
+
+export default emojipedia;
